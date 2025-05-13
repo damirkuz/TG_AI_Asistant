e594c14711d0c37fb5e8eab6e282e0b021b6d3a8
Damir Kuzdikenov <damirkuzdikenov@yandex.ru>
2025-05-14 01:26:44 +0300

    Сделать доступным получение telethon TelegramClient

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import asyncio
 
 from config_data import load_config, Config
 from tg_bot.main import start_tg_bot
+from async_logging import setup_async_logging
 
 
 async def main():
+    # настраиваем асинхронное логгирование
+    setup_async_logging()
+    # загружаем конфиг
     config: Config = load_config()
+    # запускаем тг бота
     await start_tg_bot(config=config)
 
 
