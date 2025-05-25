0994c279a8809ecc6774d8cc7c846fd436094e02
Vinogradov Dmitriy <vinogradovdima123456789@gmail.com>
2025-05-25 19:20:20 +0300

    Содать файл - датасет из спам сообщений для spam_filter.py

--- neural_networks/text_preprocessing/spam_filter/parser_dialog_dataset.py
+++
@@ -1,4 +0,0 @@
-
-PATH_DIALOG_DATASET = "C:/Users/UserHome/PycharmProjects/ProjectKom/TG_AI_Asistant/neural_networks/text_preprocessing/spam_filter/jokes_dialog_dataset.json"
-def parser_dialog_dataset(path: str = PATH_DIALOG_DATASET):
-    pass
